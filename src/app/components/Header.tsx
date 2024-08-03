d2e35ff5d1d4362308765c17c73ac0edba4d67ae
Jane Maingi <janemaingi2@gmail.com>
2024-08-03 20:23:38 +0300

    added authetication

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,16 @@
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
 import Link from "next/link";
 
-export default function Header() {
+export default async function Header() {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
+  const signOutUrl = await getSignUpUrl();
+
   return (
     <header>
       <div
@@ -10,11 +20,32 @@ export default function Header() {
         <Link href={"/"} className="font-bold text-xl">
           Job Board
         </Link>
-        <nav className="flex gap-4 *:py-2 *:px-4 *:rounded-md">
-          <Link href={"/login"} className="bg-gray-200">
-            Login
-          </Link>
-          <Link href={"/new-listing"} className="bg-blue-600 text-white">
+        <nav className="flex gap-2">
+          {!user && (
+            <Link href={signInUrl} className="py-2 px-4 rounded-md bg-gray-200">
+              Login
+            </Link>
+          )}
+          {user && (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              {" "}
+              <button
+                type="submit"
+                className="bg-gray-200 py-2 px-4 rounded-md"
+              >
+                Logout
+              </button>
+            </form>
+          )}
+          <Link
+            href={"/new-listing"}
+            className="py-2 px-4 rounded-md bg-blue-600 text-white"
+          >
             Post a Job
           </Link>
         </nav>
