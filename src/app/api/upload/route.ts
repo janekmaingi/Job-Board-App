f7054c34d4a60a9aa9081ab1f83336dfe9e16a8b
Jane Maingi <janemaingi2@gmail.com>
2024-08-11 23:36:54 +0300

    add image upload to s3

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,7 +1,14 @@
+import { S3Client } from "@aws-sdk/client-s3";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   const data = await req.formData();
-  const file = data.get("file");
-  return Response.json(file);
+  const file = data.get("file") as File;
+  const s3Client = new S3Client({
+    region: "eu-north-1",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    },
+  });
 }
